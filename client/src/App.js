324be1e548ca323bee476956f48b697702adb1d0
Peter Bid <peterbid21@hotmail.com>
2022-04-12 15:23:18 +0100

    deleted extra routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
 import SiteNavbar from './components/SiteNavbar'
 
 
@@ -14,9 +11,6 @@ function App() {
       <SiteNavbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/myprojects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
       </div>
